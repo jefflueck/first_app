600aaf499990b6cdb57e3b52b00f3e98df3529c6
jefflueck <lueckcfd@yahoo.com>
2024-07-12 17:17:32 -0500

    broke out text styling into seperate widget

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,3 +1,4 @@
+import 'package:first_app/styled_text.dart';
 import 'package:flutter/material.dart';
 
 class GradientContainer extends StatelessWidget {
@@ -13,13 +14,7 @@ class GradientContainer extends StatelessWidget {
         ),
       ),
       child: const Center(
-        child: Text(
-          'Hello World',
-          style: TextStyle(
-            fontSize: 30,
-            color: Colors.white,
-          ),
-        ),
+        child: StyledText(),
       ),
     );
   }
