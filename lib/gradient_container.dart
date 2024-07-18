fd6948dce8a94b2299226b5f45db2da82e70c2f6
jefflueck <lueckcfd@yahoo.com>
2024-07-18 16:51:47 -0500

    added dart:math to make our custom widget generate a random number, to then pull a random dice img when user clicks roll dice method. App complete

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,30 +1,8 @@
-import 'package:first_app/styled_text.dart';
 import 'package:flutter/material.dart';
+import 'package:first_app/dice_roller.dart';
 
 const endAlignment = Alignment.bottomRight;
 const beginAlignment = Alignment.topLeft;
-
-//  * This one way to break out the colors into a separate class
-// class GradientContainer extends StatelessWidget {
-// const GradientContainer({super.key, required this.colors});
-
-// final List<Color> colors;
-
-// @override
-// Widget build(context) {
-//   return Container(
-//     decoration: BoxDecoration(
-//       gradient: LinearGradient(
-//         colors: colors,
-//         begin: beginAlignment,
-//         end: endAlignment,
-//       ),
-//     ),
-//     child: const Center(
-//       child: StyledText('Hello, World!'),
-//     ),
-//   );
-// }
 
 //  * This is another way to break out the colors into a separate class
 class GradientContainer extends StatelessWidget {
@@ -44,7 +22,9 @@ class GradientContainer extends StatelessWidget {
         ),
       ),
       child: const Center(
-        child: StyledText('Hello, World!'),
+        child: DiceRoller(
+          key: Key('dice-roller'),
+        ),
       ),
     );
   }
